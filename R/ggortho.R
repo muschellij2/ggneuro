9d4e9b32de9bb7a2b4a41e2061e37ce52a46d4d8
muschellij2 <muschellij2@gmail.com>
2017-09-21 12:06:27 -0400

    Trying some interactive graphics.

--- R/ggortho.R
+++ R/ggortho.R
@@ -4,8 +4,14 @@
 #'
 #' @param img object that can be coerced to a \code{nifti} using
 #' \code{\link{check_nifti}}
+#' @param overlay object that can be coerced to a \code{nifti} using
+#' \code{\link{check_nifti}}
 #' @param xyz coordinates to be plotted
 #' @param crosshairs should crosshairs be added?
+#' @param col Colors to map intensities for \code{img},
+#' passed to \code{\link{img_colour_df}}
+#' @param col.overlay Colors to map intensities for \code{overlay},
+#' passed to \code{\link{img_colour_df}}
 #' @param ... arguments to pass to \code{\link{img_colour_df}}
 #'
 #' @return \code{construct_ggortho} returns a list of the constructed plot,
@@ -23,25 +29,49 @@
 #' @importFrom neurobase check_nifti slice_colour_df check_nifti img_colour_df
 #' @importFrom ggplot2 ggplot geom_tile facet_wrap scale_fill_identity aes aes_string
 #' @importFrom ggplot2 geom_hline geom_vline
-construct_ggortho = function(img, xyz = NULL, crosshairs = TRUE, ...) {
-  img = check_nifti(img)
-  img_df = img_colour_df(img = img, ...)
+construct_ggortho = function(
+  img, overlay = NULL,
+  xyz = NULL, crosshairs = TRUE,
+  col = gray(0:64/64),
+  col.overlay = oro.nifti::hotmetal(),
+  ...) {
+
+  img = check_nifti(img, allow.array = TRUE)
+  img_df = img_colour_df(img = img, col = col, ...)
+
+  if (!is.null(overlay)) {
+    overlay = check_nifti(overlay, allow.array = TRUE)
+    overlay[ overlay == 0 ] = NA
+    overlay_df = img_colour_df(
+      img = overlay,
+      col = col.overlay, ...)
+  }
+
   if (is.null(xyz)) {
     xyz = ceiling(dim(img) / 2)
   }
   slice_df = slice_colour_df(img_df, xyz = xyz)
-  out = ggortho_slice_df(slice_df, crosshairs = crosshairs)
-  L = list(plot = out,
-           df = img_df,
-           sliced_df = slice_df,
-           xyz = xyz)
+  if (!is.null(overlay)) {
+    overlay_slice_df = slice_colour_df(overlay_df, xyz = xyz)
+  } else {
+    overlay_slice_df = NULL
+  }
+  out = ggortho_slice_df(
+    slice_df,
+    slice_overlay_df = overlay_slice_df,
+    crosshairs = crosshairs)
+  L = list(
+    plot = out,
+    df = img_df,
+    sliced_df = slice_df,
+    xyz = xyz)
   return(L)
 }
 
 #' @rdname ggortho
 #' @export
-ggortho = function(img, xyz = NULL, crosshairs = TRUE, ...) {
-  L = construct_ggortho(img = img, xyz = xyz, ...)
+ggortho = function(img, overlay = NULL, xyz = NULL, crosshairs = TRUE, ...) {
+  L = construct_ggortho(img = img, overlay = overlay, xyz = xyz, ...)
   return(L$plot)
 }
 
@@ -56,26 +86,59 @@ ggortho2 = function(...) {
 #' @rdname ggortho
 #' @param img_df Image \code{data.frame} constructed from \code{\link{img_colour_df}}
 #' @export
-ggortho_img_df = function(img_df, xyz = NULL, crosshairs = TRUE) {
+ggortho_img_df = function(
+  img_df,
+  overlay_df = NULL,
+  xyz = NULL,
+  crosshairs = TRUE) {
+
   slice_df = slice_colour_df(img_df, xyz = xyz)
-  return(ggortho_slice_df(slice_df, crosshairs = crosshairs))
+  if (!is.null(overlay_df)) {
+    slice_overlay_df = slice_colour_df(overlay_df, xyz = xyz)
+  }
+  return(ggortho_slice_df(
+    slice_df = slice_df,
+    slice_overlay_df = slice_overlay_df,
+    crosshairs = crosshairs))
+}
+
+
+#' @rdname ggortho
+#' @export
+ggortho_base = function() {
+  out = ggplot() + facet_wrap(~ plane2, nrow = 2, ncol = 2,
+                              scales = "free")
+  out = out + theme_ortho() + scale_fill_identity()
+  out
 }
 
 #' @rdname ggortho
 #' @param slice_df Image \code{data.frame} constructed from \code{\link{slice_colour_df}}
+#' @param slice_overlay_df Image \code{data.frame} constructed from
+#' \code{\link{slice_colour_df}} to overlay
+#'
+#' @importFrom ggplot2 "%+%"
 #' @export
-ggortho_slice_df = function(slice_df, crosshairs = TRUE) {
-  out = ggplot(
-    data = slice_df,
-    aes_string(x = "x", y = "y", fill = "colour")) +
-    geom_tile() +
-    facet_wrap(~ plane2, nrow = 2, ncol = 2,
-               scales = "free")
-  out = out + theme_ortho() +
-    scale_fill_identity()
+ggortho_slice_df = function(
+  slice_df,
+  slice_overlay_df = NULL,
+  crosshairs = TRUE) {
+
+  out = ggortho_base()
+  out = out %+% slice_df
+  out = out + geom_tile(aes_string(x = "x", y = "y", fill = "colour"))
+  if (!is.null(slice_overlay_df)) {
+    out = out + geom_tile(
+      data = slice_overlay_df,
+      aes_string(
+        x = "x",
+        y = "y",
+        fill = "colour"))
+  }
   if (crosshairs) {
-    int_df = unique(slice_df[, c("plane", "plane2",
-                                 "xintercept", "yintercept")])
+    int_df = unique(
+      slice_df[, c("plane", "plane2",
+                   "xintercept", "yintercept")])
     out = out +
       geom_vline(data = int_df,
                  aes_string(xintercept = "xintercept"),
@@ -83,7 +146,7 @@ ggortho_slice_df = function(slice_df, crosshairs = TRUE) {
       geom_hline(data = int_df,
                  aes_string(yintercept = "yintercept"),
                  colour = "red")
-
   }
   return(out)
 }
+
